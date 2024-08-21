6430a94d394745dd56b09f55cb5181871e4f87c0
andrxpie <andrxpie@gmail.com>
2024-08-21 20:25:03 +0300

    All invoices page, home page, edit invoice page

---
+++ PhoneBook/src/main/java/org/example/model/Invoice.java
@@ -0,0 +1,26 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private Double amount;
+}
